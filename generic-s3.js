0e4a7443cb820e4d8f485333497ee44c851d03fd
rristov60 <risteristov60@yahoo.com>
2023-01-06 09:38:41 +0100

    Added optional upsert for Put Object

--- generic-s3.js
+++ generic-s3.js
@@ -377,7 +377,9 @@ module.exports = function(RED) {
             let body = n.body !== "" ? n.body : null; // Body of the object to upload
             let metadata = n.metadata !== "" ? n.metadata : null; // Metadata of the object
             let contentType = n.contentType !== "" ? n.contentType : null; // Content-Type of the object
-            
+            let upsert = n.upsert ? n.upsert : false;
+
+
             // Checking for correct properties input
             if(!bucket) {
                 bucket = msg.bucket ? msg.bucket : null;
@@ -434,6 +436,10 @@ module.exports = function(RED) {
                 
             }
 
+            if(!upsert) {
+                upsert = msg.upsert ? msg.upsert : false;
+            }
+
             // S3 client init
             let s3Client = null;
 
@@ -461,65 +467,149 @@ module.exports = function(RED) {
                     return;
                 }
 
-                // Calculating MD5 od the body
-                const MD5 = crypto.createHash('md5').update(body).digest("hex");
-
-                // Fetching HeadData (Metadata) for the object that is being upserted or inserted
-                let objectMeta = {};
-                try{
-                    objectMeta = await s3Client.headObject({
-                        Bucket: bucket,
-                        Key: key
-                    });
-                    let ETag = objectMeta.ETag.substring(1, objectMeta.ETag.length - 1); // Formatting the ETag
-                    
-                    // Checking if the existing object data is exactly the same as the request message
-                    if(ETag == MD5) {
-                        node.warn(`The object ${key} has not been upserted since the body of the existing object is exactly the same`);
-                        send({payload: null, key: key});
-                        if(done) done();
-                        return;
-                    }
-                } catch (e) {
-                    // If the object does not exist, continue with inserting
-                }
-
-                // Creating the upload object
-                let objectToCreate = {
-                    Bucket: bucket,
-                    Key: key,
-                    ContentType: contentType,
-                    Body: streamifiedBody
-                };
+                if(upsert) {
+                    // Calculating MD5 od the body
+                    const MD5 = crypto.createHash('md5').update(body).digest("hex");
+    
+                    // Fetching HeadData (Metadata) for the object that is being upserted or inserted
+                    try{
+                        s3Client.headObject({ Bucket: bucket, Key: key }, function(err, data) {
+                            if(err) {
+                                // Creating the upload object
+                                let objectToCreate = {
+                                    Bucket: bucket,
+                                    Key: key,
+                                    ContentType: contentType,
+                                    Body: streamifiedBody
+                                };
+                                
+                                if(metadata) objectToCreate.Metadata = metadata;
                 
-                if(metadata) objectToCreate.Metadata = metadata;
+                                // Uploading
+                                node.status({fill:"blue",shape:"dot",text:"Uploading"});
+                                s3Client.putObject(objectToCreate, function(error, data) {
+                                    // If an error occured, print the error
+                                    if(error) {
+                                        node.status({fill:"red",shape:"dot",text:`Failure`});
+                                        node.error(error);
+                                        send({payload: null, key: key});
+                                    } else {  // if not, return message with the payload
+                                        send({
+                                            payload: data,
+                                            key: key
+                                        }); 
+                                        node.status({fill:"green",shape:"dot",text:`Success`});
+                                    }
+                
+                                    // Finalize
+                                    if(done) {
+                                        s3Client.destroy();
+                                        done();
+                                    }
+                                    // Clear node's status
+                                    setTimeout(() => {
+                                        node.status({});
+                                    }, 5000);
+                                });
+                            } else {
+                                let ETag = data.ETag.substring(1, data.ETag.length - 1); // Formatting the ETag
+                                // Checking if the existing object data is exactly the same as the request message
+                                if(ETag == MD5) {
+                                    node.warn(`The object ${key} has not been upserted since the body of the existing object is exactly the same`);
+                                    send({payload: null, key: key});
+                                    if(done) done();
+                                    return;
+                                } else {
+                                    // Creating the upload object
+                                    let objectToCreate = {
+                                        Bucket: bucket,
+                                        Key: key,
+                                        ContentType: contentType,
+                                        Body: streamifiedBody
+                                    };
+                                    
+                                    if(metadata) objectToCreate.Metadata = metadata;
+                    
+                                    // Uploading
+                                    node.status({fill:"blue",shape:"dot",text:"Uploading"});
+                                    s3Client.putObject(objectToCreate, function(error, data) {
+                                        // If an error occured, print the error
+                                        if(error) {
+                                            node.status({fill:"red",shape:"dot",text:`Failure`});
+                                            node.error(error);
+                                            send({payload: null, key: key});
+                                        } else {  // if not, return message with the payload
+                                            send({
+                                                payload: data,
+                                                key: key
+                                            }); 
+                                            node.status({fill:"green",shape:"dot",text:`Success`});
+                                        }
+                    
+                                        // Finalize
+                                        if(done) {
+                                            s3Client.destroy();
+                                            done();
+                                        }
+                                        // Clear node's status
+                                        setTimeout(() => {
+                                            node.status({});
+                                        }, 5000);
+                                    });
+                                }
+                            }
+                        });
 
-                // Uploading
-                node.status({fill:"blue",shape:"dot",text:"Uploading"});
-                s3Client.putObject(objectToCreate, function(error, data) {
-                    // If an error occured, print the error
-                    if(error) {
-                        node.status({fill:"red",shape:"dot",text:`Failure`});
-                        node.error(error);
-                        send({payload: null, key: key});
-                    } else {  // if not, return message with the payload
-                        send({
-                            payload: data,
-                            key: key
-                        }); 
-                        node.status({fill:"green",shape:"dot",text:`Success`});
+                    } catch (err) {
+                         // If error occurs
+                        node.error(err);
+                        // Cleanup
+                        if(s3Client !== null) s3Client.destroy();
+                        if(done) done();
+                        
+                        node.status({fill:"red",shape:"dot",text:"Failure"});
+                        setTimeout(() => {
+                            node.status({});
+                        }, 5000);
                     }
-
-                    // Finalize
-                    if(done) {
-                        s3Client.destroy();
-                        done();
-                    }
-                    // Clear node's status
-                    setTimeout(() => {
-                        node.status({});
-                    }, 5000);
-                });
+                } else {
+                    // Creating the upload object
+                    let objectToCreate = {
+                        Bucket: bucket,
+                        Key: key,
+                        ContentType: contentType,
+                        Body: streamifiedBody
+                    };
+                    
+                    if(metadata) objectToCreate.Metadata = metadata;
+    
+                    // Uploading
+                    node.status({fill:"blue",shape:"dot",text:"Uploading"});
+                    s3Client.putObject(objectToCreate, function(error, data) {
+                        // If an error occured, print the error
+                        if(error) {
+                            node.status({fill:"red",shape:"dot",text:`Failure`});
+                            node.error(error);
+                            send({payload: null, key: key});
+                        } else {  // if not, return message with the payload
+                            send({
+                                payload: data,
+                                key: key
+                            }); 
+                            node.status({fill:"green",shape:"dot",text:`Success`});
+                        }
+    
+                        // Finalize
+                        if(done) {
+                            s3Client.destroy();
+                            done();
+                        }
+                        // Clear node's status
+                        setTimeout(() => {
+                            node.status({});
+                        }, 5000);
+                    });
+                }
             }
             catch (err) {
                 // If error occurs
