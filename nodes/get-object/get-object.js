d4f583ce4b06cc166afd6a65b39e0b933ac582ef
Riste Ristov <63927203+rristov60@users.noreply.github.com>
2023-04-18 17:35:14 +0200

    Fix stripping msg objects (#40)
    * Fixed msg stripping in copy-object
    
    * Fixed msg stripping in create-bucket
    
    * Fixed msg stripping in delete-object
    
    * Fixed msg stripping in get-object
    
    * Fixed msg stripping in head-object
    
    * Fixed msg stripping in list-buckets
    
    * Fixed msg stripping in list-object-versions
    
    * Fixed msg stripping in list-objects
    
    * Fixed msg stripping in list-objects-v2
    
    * Fixed msg stripping in put-object
    
    * Fixed msg stripping in put-objects
    
    * Updated CHANGELOG.md

--- nodes/get-object/get-object.js
+++ nodes/get-object/get-object.js
@@ -1,122 +1,132 @@
-module.exports = function(RED) {
-    "use strict";
-    const { S3 } = require('@aws-sdk/client-s3');
-    const { streamToString } = require('../../common/common');
+module.exports = function (RED) {
+  "use strict";
+  const { S3 } = require("@aws-sdk/client-s3");
+  const { streamToString } = require("../../common/common");
 
-    // Get Object
-    function S3GetObject(n) {
-        RED.nodes.createNode(this,n); // Getting options for the current node
-        this.conf = RED.nodes.getNode(n.conf); // Getting configuration
-        var node = this; // Referencing the current node
-        var config = this.conf ? this.conf : null; // Cheking if the conf is valid
+  // Get Object
+  function S3GetObject(n) {
+    RED.nodes.createNode(this, n); // Getting options for the current node
+    this.conf = RED.nodes.getNode(n.conf); // Getting configuration
+    var node = this; // Referencing the current node
+    var config = this.conf ? this.conf : null; // Cheking if the conf is valid
 
-        if (!config) {
-            node.warn(RED._("Missing S3 Client Configuration!"));
-            return;
-        }
-
-        this.on('input', async function(msg, send, done) {
-
-            let bucket = n.bucket != "" ? n.bucket : null;
-            let key = n.key != "" ? n.key : null;
-
-            let getObjectPayload = {};
-
-            // Checking for correct properties input
-            if(!bucket) {
-                bucket = msg.bucket ? msg.bucket : null;
-                if(!bucket) {
-                    node.error('No bucket provided!');
-                    return;
-                }
-            }
-            getObjectPayload.Bucket = bucket;
-
-            if(!key) {
-                key = msg.key ? msg.key : null;
-                if(!key) {
-                    node.error('No object key provided!');
-                    return;
-                }
-            }
-            getObjectPayload.Key = key;
-
-            // versionId parameter
-            let versionid = n.versionid != "" ? n.versionid : null;
-            if(!versionid) {
-                versionid = msg.versionid ? msg.versionid : null;
-            }
-            if(versionid) {
-                getObjectPayload.VersionId = versionid;
-            }
-
-            // stringifyBody parameter
-            let stringifybody = n.stringifybody ? n.stringifybody : false;
-            if(!stringifybody) {
-                stringifybody = msg.stringifybody ? msg.stringifybody : false;
-            }
-
-            // S3 client init
-            let s3Client = null;
-            try {
-                // Creating S3 client
-                s3Client = new S3({
-                    endpoint: config.endpoint,
-                    region: config.region,
-                    credentials: {
-                        accessKeyId: config.credentials.accesskeyid,
-                        secretAccessKey: config.credentials.secretaccesskey
-                    }
-                });
-
-                node.status({fill:"blue",shape:"dot",text:"Fetching"});
-
-                s3Client.getObject(getObjectPayload, async function(err, data) {
-                    // If an error occured, print the error
-                    if(err) {
-                        node.status({fill:"red",shape:"dot",text:`Failure`});
-                        node.error(err);
-                        send({payload: null, key: key});
-                    } else { // if not, return message with the payload
-
-                        if(stringifybody) {
-                            data.BodyAsString = await streamToString(data.Body);
-                        }
-
-                        send({
-                            payload: data,
-                            key: key
-                        });
-                        node.status({fill:"green",shape:"dot",text:"Success"});
-                    }
-
-                    // Finalize
-                    if(done) {
-                        s3Client.destroy();
-                        done();
-                    }
-
-                    setTimeout(() => {
-                        node.status({});
-                    }, 2000);
-
-                });
-
-            }
-            catch (err) {
-                // If error occurs
-                node.error(err);
-                // Cleanup
-                if(s3Client !== null) s3Client.destroy();
-                if(done) done();
-
-                node.status({fill:"red",shape:"dot",text:"Failure"});
-                setTimeout(() => {
-                    node.status({});
-                }, 3000);
-            }
-        });
+    if (!config) {
+      node.warn(RED._("Missing S3 Client Configuration!"));
+      return;
     }
 
-    RED.nodes.registerType('Get Object', S3GetObject);
-}
+    this.on("input", async function (msg, send, done) {
+      let bucket = n.bucket != "" ? n.bucket : null;
+      let key = n.key != "" ? n.key : null;
+
+      let getObjectPayload = {};
+
+      // Checking for correct properties input
+      if (!bucket) {
+        bucket = msg.bucket ? msg.bucket : null;
+        if (!bucket) {
+          node.error("No bucket provided!");
+          return;
+        }
+      }
+      getObjectPayload.Bucket = bucket;
+
+      if (!key) {
+        key = msg.key ? msg.key : null;
+        if (!key) {
+          node.error("No object key provided!");
+          return;
+        }
+      }
+      getObjectPayload.Key = key;
+
+      // versionId parameter
+      let versionid = n.versionid != "" ? n.versionid : null;
+      if (!versionid) {
+        versionid = msg.versionid ? msg.versionid : null;
+      }
+      if (versionid) {
+        getObjectPayload.VersionId = versionid;
+      }
+
+      // stringifyBody parameter
+      let stringifybody = n.stringifybody ? n.stringifybody : false;
+      if (!stringifybody) {
+        stringifybody = msg.stringifybody ? msg.stringifybody : false;
+      }
+
+      // S3 client init
+      let s3Client = null;
+      try {
+        // Creating S3 client
+        s3Client = new S3({
+          endpoint: config.endpoint,
+          region: config.region,
+          credentials: {
+            accessKeyId: config.credentials.accesskeyid,
+            secretAccessKey: config.credentials.secretaccesskey,
+          },
+        });
+
+        node.status({ fill: "blue", shape: "dot", text: "Fetching" });
+
+        s3Client.getObject(getObjectPayload, async function (err, data) {
+          // If an error occured, print the error
+          if (err) {
+            node.status({ fill: "red", shape: "dot", text: `Failure` });
+            node.error(err);
+            // Replace the payload with null
+            msg.payload = null;
+            // Append the object
+            // key to the message object
+            msg.key = key;
+
+            // Return the complete message object
+            send(msg);
+          } else {
+            // if not, return message with the payload
+
+            if (stringifybody) {
+              data.BodyAsString = await streamToString(data.Body);
+            }
+
+            // Replace the payload with
+            // the returned data
+            msg.payload = data;
+            // Append the object
+            // key to the message object
+            msg.key = key;
+
+            // Return the complete message object
+            send(msg);
+
+            node.status({ fill: "green", shape: "dot", text: "Success" });
+          }
+
+          // Finalize
+          if (done) {
+            s3Client.destroy();
+            done();
+          }
+
+          setTimeout(() => {
+            node.status({});
+          }, 2000);
+        });
+      } catch (err) {
+        // If error occurs
+        node.error(err);
+        // Cleanup
+        if (s3Client !== null) s3Client.destroy();
+        if (done) done();
+
+        node.status({ fill: "red", shape: "dot", text: "Failure" });
+        setTimeout(() => {
+          node.status({});
+        }, 3000);
+      }
+    });
+  }
+
+  RED.nodes.registerType("Get Object", S3GetObject);
+};
--- nodes/list-buckets/list-buckets.js
+++ nodes/list-buckets/list-buckets.js
@@ -1,74 +1,78 @@
-module.exports = function(RED) {
-    "use strict";
-    const { S3 } = require('@aws-sdk/client-s3');
+module.exports = function (RED) {
+  "use strict";
+  const { S3 } = require("@aws-sdk/client-s3");
 
-    // LIST BUCKETS NODE
-    function S3ListBuckets(n) {
-        RED.nodes.createNode(this,n); // Getting options for the current node
-        this.conf = RED.nodes.getNode(n.conf); // Getting configuration
-        var node = this; // Referencing the current node
-        var config = this.conf ? this.conf : null; // Cheking if the conf is valid
+  // LIST BUCKETS NODE
+  function S3ListBuckets(n) {
+    RED.nodes.createNode(this, n); // Getting options for the current node
+    this.conf = RED.nodes.getNode(n.conf); // Getting configuration
+    var node = this; // Referencing the current node
+    var config = this.conf ? this.conf : null; // Cheking if the conf is valid
 
-        if (!config) {
-            node.warn(RED._("Missing S3 Client Configuration!"));
-            return;
-        }
-
-        // Make the handler for the input event async
-        this.on("input", async function(msg,send,done) {
-
-            // S3 client init
-            let s3Client = null;
-            try {
-                // Creating S3 client
-                s3Client = new S3({
-                    endpoint: config.endpoint,
-                    region: config.region,
-                    credentials: {
-                        accessKeyId: config.credentials.accesskeyid,
-                        secretAccessKey: config.credentials.secretaccesskey
-                    }
-                });
-
-                node.status({fill:"blue",shape:"dot",text:"Fetching"});
-                // Listing all the buckets and formatting the message
-                s3Client.listBuckets({}, function(err, data) {
-                    if(err) {
-                        node.status({fill:"red",shape:"dot",text:`Failure`});
-                        node.error(err);
-                        send({payload: null});
-                    } else {
-                        // Sending the message
-                        send({
-                            payload: data
-                        });
-                    }
-                    // Finalizing
-                    if(done) {
-                        s3Client.destroy();
-                        done();
-                    }
-
-                    node.status({fill:"green",shape:"dot",text:"Success"});
-                    setTimeout(() => {
-                        node.status({});
-                    }, 2000);
-                });
-            }
-            catch (err) {
-                // If an error occurs
-                node.error(err);
-                // Cleanup
-                if(s3Client !== null) s3Client.destroy();
-                if(done) done();
-
-                node.status({fill:"red",shape:"dot",text:"Failure"});
-                setTimeout(() => {
-                    node.status({});
-                }, 3000);
-            }
-        });
+    if (!config) {
+      node.warn(RED._("Missing S3 Client Configuration!"));
+      return;
     }
 
-    RED.nodes.registerType("List Buckets", S3ListBuckets);
-}
+    // Make the handler for the input event async
+    this.on("input", async function (msg, send, done) {
+      // S3 client init
+      let s3Client = null;
+      try {
+        // Creating S3 client
+        s3Client = new S3({
+          endpoint: config.endpoint,
+          region: config.region,
+          credentials: {
+            accessKeyId: config.credentials.accesskeyid,
+            secretAccessKey: config.credentials.secretaccesskey,
+          },
+        });
+
+        node.status({ fill: "blue", shape: "dot", text: "Fetching" });
+        // Listing all the buckets and formatting the message
+        s3Client.listBuckets({}, function (err, data) {
+          if (err) {
+            node.status({ fill: "red", shape: "dot", text: `Failure` });
+            node.error(err);
+            // Replace the payload with null
+            msg.payload = null;
+
+            // Return the complete message object
+            send(msg);
+          } else {
+            // Replace the payload with
+            // the returned data
+            msg.payload = data;
+
+            // Return the complete message object
+            send(msg);
+          }
+          // Finalizing
+          if (done) {
+            s3Client.destroy();
+            done();
+          }
+
+          node.status({ fill: "green", shape: "dot", text: "Success" });
+          setTimeout(() => {
+            node.status({});
+          }, 2000);
+        });
+      } catch (err) {
+        // If an error occurs
+        node.error(err);
+        // Cleanup
+        if (s3Client !== null) s3Client.destroy();
+        if (done) done();
+
+        node.status({ fill: "red", shape: "dot", text: "Failure" });
+        setTimeout(() => {
+          node.status({});
+        }, 3000);
+      }
+    });
+  }
+
+  RED.nodes.registerType("List Buckets", S3ListBuckets);
+};
