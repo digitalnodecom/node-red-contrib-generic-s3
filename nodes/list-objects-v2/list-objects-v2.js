cb84a8cc81b84703cf71581b9980e37c16cd790c
rristov60 <risteristov60@yahoo.com>
2023-08-01 15:22:40 +0200

    Patched s3Client.destroy() in list-objects-v2.js

--- nodes/list-objects-v2/list-objects-v2.js
+++ nodes/list-objects-v2/list-objects-v2.js
@@ -122,7 +122,7 @@ function inputHandler(n, RED) {
       msg.bucket = payloadConfig.Bucket;
       send(msg);
     } finally {
-      s3Client.destroy();
+      if(s3Client) s3Client.destroy();
       /* Dereference vars */
       s3Client = null;
       /*********************/
