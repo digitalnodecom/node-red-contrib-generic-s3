c971951d56c638694d2a4cfd7dd51a1d86020094
rristov60 <risteristov60@yahoo.com>
2023-08-01 15:22:49 +0200

    Patched s3Client.destroy() in list-object-versions.js

--- nodes/list-object-versions/list-object-versions.js
+++ nodes/list-object-versions/list-object-versions.js
@@ -120,7 +120,7 @@ function inputHandler(n, RED) {
       msg.bucket = payloadConfig.Bucket;
       send(msg);
     } finally {
-      s3Client.destroy();
+      if(s3Client) s3Client.destroy();
       /* Dereference vars */
       s3Client = null;
       /*********************/
